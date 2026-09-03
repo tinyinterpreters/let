89b69e055519d0a10f3962077a9760b40dd2e918
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-09-03 16:51:34 -0400

    Test the interpreter to ensure that the let expressions work as expected

--- tests/Test/LET/Interpreter.elm
+++ tests/Test/LET/Interpreter.elm
@@ -119,6 +119,98 @@ suite =
                 , ( "x", SucceedsWith (VNumber 10) )
                 , ( "if zero?(-(5, v)) then i else v", SucceedsWith (VNumber 1) )
                 , ( "y", RuntimeError <| I.IdentifierNotFound "y" )
+
+                -- Let expressions
+                , ( "let a = 5 in a"
+                  , SucceedsWith (VNumber 5)
+                  )
+                , ( "let a = 5 in -(a, 3)"
+                  , SucceedsWith (VNumber 2)
+                  )
+
+                --- Binding the results of expressions
+                , ( "let a = -(5, 3) in zero?(a)"
+                  , SucceedsWith (VBool False)
+                  )
+                , ( "let a = zero?(0) in if a then 2 else 3"
+                  , SucceedsWith (VNumber 2)
+                  )
+
+                --- Nested let expressions
+                , ( "let a = 5 in let b = 3 in -(a, b)"
+                  , SucceedsWith (VNumber 2)
+                  )
+                , ( "let a = 5 in let b = -(a, 2) in -(a, b)"
+                  , SucceedsWith (VNumber 2)
+                  )
+                , ( """
+                    let x = 33
+                    in let y = 22
+                       in if zero?(-(x, 11)) then -(y, 2) else -(y, 4)
+                    """
+                  , SucceedsWith (VNumber 18)
+                  )
+
+                --- Shadowing
+                , ( "let a = 5 in let a = 3 in a"
+                  , SucceedsWith (VNumber 3)
+                  )
+
+                --- The outer binding remains available after evaluating the inner let
+                , ( "let a = 5 in -(let a = 3 in a, a)"
+                  , SucceedsWith (VNumber -2)
+                  )
+                , ( """
+                    let z = 5 in
+                      let x = 3 in
+                        let y = -(x, 1) in
+                          let x = 4 in -(z, -(x, y))
+                    """
+                  , SucceedsWith (VNumber 3)
+                  )
+
+                --- Bound expressions use the surrounding environment
+                , ( """
+                    let x = 7 in
+                      let y = 2 in
+                        let y = let x = -(x, 1) in -(x, y) in
+                          -(-(x, 8), y)
+                    """
+                  , SucceedsWith (VNumber -5)
+                  )
+
+                --- Errors while evaluating the bound expression
+                , ( "let a = zero?(zero?(0)) in a"
+                  , RuntimeError <|
+                        I.TypeError
+                            { expected = [ I.TNumber ]
+                            , actual = [ I.TBool ]
+                            }
+                  )
+                , ( "let a = missing in a"
+                  , RuntimeError <| I.IdentifierNotFound "missing"
+                  )
+
+                --- Errors while evaluating the body
+                , ( "let a = zero?(0) in -(a, 1)"
+                  , RuntimeError <|
+                        I.TypeError
+                            { expected = [ I.TNumber, I.TNumber ]
+                            , actual = [ I.TBool, I.TNumber ]
+                            }
+                  )
+                , ( "let a = 5 in missing"
+                  , RuntimeError <| I.IdentifierNotFound "missing"
+                  )
+
+                --- The bound expression is evaluated before the body
+                , ( "let a = zero?(zero?(0)) in missing"
+                  , RuntimeError <|
+                        I.TypeError
+                            { expected = [ I.TNumber ]
+                            , actual = [ I.TBool ]
+                            }
+                  )
                 ]
         ]
 
