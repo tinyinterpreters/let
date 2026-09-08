623b066b5ae706663283855c5862301be9cd4fd6
Dwayne Crooks <dwayne@tinyinterpreters.dev>
2026-09-08 09:08:41 -0400

    Refactor flake.nix
      - Use explicit inputs
      - Use mkElmShell from tinyinterpreters/nix
      - Update the flake inputs
      - Bump Elm version to 0.19.2

--- flake.nix
+++ flake.nix
@@ -1,49 +1,19 @@
 {
-  outputs = { self, nixpkgs, flake-utils }:
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    ti.url = "github:tinyinterpreters/nix";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ti }:
     flake-utils.lib.eachDefaultSystem(system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        mkElmShell = ti.lib.mkElmShell pkgs;
       in
       {
-        devShells.default = pkgs.mkShell {
+        devShells.default = mkElmShell {
           name = "let";
-
-          packages = [
-            pkgs.elmPackages.elm
-            pkgs.elmPackages.elm-format
-            pkgs.elmPackages.elm-test
-          ];
-
-          shellHook = ''
-            export PROJECT_ROOT="$(git rev-parse --show-toplevel)"
-            export PS1="($name)\n$PS1"
-
-            f () {
-              elm-format "$PROJECT_ROOT/"{src,tests} "''${@:---yes}"
-            }
-
-            t () {
-              elm-test "$@"
-            }
-
-            c () {
-              nix flake check -L &&
-              f --validate &&
-              t
-            }
-
-            clean () {
-              rm -rf "$PROJECT_ROOT/elm-stuff"
-            }
-
-            echo "Development environment loaded"
-            echo ""
-            echo "Type 'f' to run elm-format"
-            echo "Type 't' to run elm-test"
-            echo "Type 'c' to run all checks"
-            echo "Type 'clean' to remove build artifacts"
-            echo ""
-          '';
         };
       }
     );
